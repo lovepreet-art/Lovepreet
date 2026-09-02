09cfe3cb8f7b03389f8b73a803a7752e10aa5168
lovepreet-art <quxelp@gmail.com>
2026-09-03 00:01:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", () => {});
